3db7bf7e7e32ee2768fe55178eaceb27113be9a6
Devashish Chakraborty <devashishchakra@gmail.com>
2025-03-31 22:43:37 +0530

    Changed loading circle

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,5 +1,5 @@
 export default function Loading() {
   return (
-    <div className="h-5 w-5 animate-spin rounded-full border-3 border-gray-300 border-t-red-800"></div>
+    <div className="h-6 w-6 animate-spin rounded-full border-3 border-gray-300 border-t-red-800"></div>
   );
 }
